c5632780849bd0c7361f19265be52b9099b2ec37
Gabriela Hdz <gabriela31hdz8@gmail.com>
2021-07-31 23:31:59 -0500

    ch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    apiKey: 'AIzaSyAFZdTQR3ILawkg6qZmHXWuQNYt2vlwCCE',
+    authDomain: 'animas-app-444d7.firebaseapp.com',
+    databaseURL:
+      'https://animas-app-444d7-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'animas-app-444d7',
+    storageBucket: 'animas-app-444d7.appspot.com',
+    messagingSenderId: '1057510856038',
+    appId: '1:1057510856038:web:e5e7033475739e63d824a7',
+    measurementId: 'G-ZNNTXP1GX3',
+  },
 };
 
 /*
